f03a773713d6477f5b74d800b479ba284bd67285
Aftab pathan <pathanaftab536@gmail.com>
2025-12-24 10:38:58 +0530

    Add Tic Tac Toe game source code

---
+++ app.js
@@ -0,0 +1,57 @@
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turn0 = true; 
+
+const winpatterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
+const checkWinner = () => {
+    for (let pattern of winpatterns) {
+        let [a, b, c] = pattern;
+        if (
+            boxes[a].innerText !== "" &&
+            boxes[a].innerText === boxes[b].innerText &&
+            boxes[b].innerText === boxes[c].innerText
+        ) {
+            alert(`${boxes[a].innerText} wins!`
+                
+            );
+            disableAllBoxes();
+            return;
+        }
+    }
+    if([...boxes].every((box) => box.innerText !== "")) {
+        alert("It's a draw!");
+    }
+};
+const disableAllBoxes = () => {
+    boxes.forEach((box) => (box.disabled = true));
+};
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (box.innerText === "") {
+            box.innerText = turn0 ? "O" : "X";
+            turn0 = !turn0; 
+            checkWinner(); 
+        }
+    });
+});
+
+
+resetBtn.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.innerText = "";
+        box.disabled = false;
+    });
+    turn0 = true; 
+});
